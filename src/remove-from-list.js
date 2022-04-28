4014dacea07d8faae2b51c2a804c5503d9dbd1a1
asya2320 <64478347+asya2320@users.noreply.github.com>
2022-04-28 22:27:19 +0300

    feat: add code

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,11 +22,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+    this.head = null;
+  }
+}
+
+function removeKFromList(l, k) {
+  let current_node=l;
+  if(l.value===k){
+    l=l.next;
+    current_node=current_node.next;
+  }
+  while(current_node){
+    while(current_node.next && current_node.next.value===k){
+      current_node.next=current_node.next.next;
+    }
+    current_node=current_node.next;
+  }
+  return l;
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
