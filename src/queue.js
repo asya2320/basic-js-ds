65fa120df89e0d1c919753b761f8b0ca8e04f10e
asya2320 <64478347+asya2320@users.noreply.github.com>
2022-04-28 22:22:48 +0300

    feat: add code

--- src/queue.js
+++ src/queue.js
@@ -13,24 +13,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class Node {
+  constructor() {
+    this.head=null;
+  }
+}
+class Queue {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    let queue_node = new Queue(value);
+    if (!this.head) {
+      this.head = queue_node;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = queue_node;
+    }
+    return queue_node;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let head_node_queue = this.head;
+    this.head = this.head.next;
+    return head_node_queue.value;
+  }
+
+  getUnderlyingList() {
+    let current = this.head;
+    return current;
   }
 }
+
 
 module.exports = {
   Queue
 };
+
